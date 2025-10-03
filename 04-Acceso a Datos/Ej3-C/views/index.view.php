69dffd6f719a5a258db00787d673772e02df36a2
Aimar Medina <142GA03@arriagainfo.local>
2025-10-03 09:17:37 +0200

    temrinado Ej3-C

--- 04-Acceso a Datos/Ej3-C/views/index.view.php
+++ 04-Acceso a Datos/Ej3-C/views/index.view.php
@@ -1,15 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <title>Document</title>
-</head>
-<body>
-    <h1>App de Gestión de Empleados</h1>
-    <p>Bienvenido a la aplicación de aprendizaje de Gestión de Empleados. Este ejericio tiene como objetivo repasar el acceso a datos pediante PDO y comenzar a separar la lógica de las páginas de la presentacón y del acceso a datos.</p>
-
+<?php require('layers/header.php') ?>
     <div class="container">
         <div class="table">
             <h1>Listado de empleados</h1>
